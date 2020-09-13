9e50b93a830b344cea44bd9302bb7ef14e4798ea
Alemiz112 <45739737+Alemiz112@users.noreply.github.com>
2020-09-13 21:33:08 +0200

    Do not block main thread by docker packets

--- src/main/java/alemiz/stargate/docker/DockerPacketHandler.java
+++ src/main/java/alemiz/stargate/docker/DockerPacketHandler.java
@@ -5,29 +5,32 @@ import alemiz.stargate.gate.GateAPI;
 import alemiz.stargate.gate.packets.ServerManagePacket;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
 
 public class DockerPacketHandler {
 
     public static void handle(ServerManagePacket packet, String client){
-        DockerConnection conn;
-
-        switch (packet.getPacketType()){
-            case ServerManagePacket.DOCKER_ADD:
-                dockerAddServer(packet, client);
-                break;
-            case ServerManagePacket.DOCKER_REMOVE:
-                if ((conn = createConnection(packet.getDockerHost())) == null) return;
-                conn.removeContainer(packet.getContainerId(), true);
-                break;
-            case ServerManagePacket.DOCKER_START:
-                if ((conn = createConnection(packet.getDockerHost())) == null) return;
-                conn.startContainer(packet.getContainerId());
-                break;
-            case ServerManagePacket.DOCKER_STOP:
-                if ((conn = createConnection(packet.getDockerHost())) == null) return;
-                conn.stopContainer(packet.getContainerId());
-                break;
+        if (packet.getPacketType() == ServerManagePacket.DOCKER_ADD){
+            dockerAddServer(packet, client);
+            return;
         }
+
+        CompletableFuture<DockerConnection> future = createConnectionAsync(packet.getDockerHost());
+        future.thenAcceptAsync(conn -> {
+            if (conn == null) return;
+
+            switch (packet.getPacketType()){
+                case ServerManagePacket.DOCKER_REMOVE:
+                    conn.removeContainer(packet.getContainerId(), true);
+                    break;
+                case ServerManagePacket.DOCKER_START:
+                    conn.startContainer(packet.getContainerId());
+                    break;
+                case ServerManagePacket.DOCKER_STOP:
+                    conn.stopContainer(packet.getContainerId());
+                    break;
+            }
+        });
     }
 
     public static DockerConnection createConnection(String dockerHost){
@@ -38,30 +41,35 @@ public class DockerPacketHandler {
         return new DockerConnection(data[0], data[1]).connect();
     }
 
-
+    public static CompletableFuture<DockerConnection> createConnectionAsync(String dockerHost){
+        return CompletableFuture.supplyAsync(() -> createConnection(dockerHost));
+    }
+    
     private static void dockerAddServer(ServerManagePacket packet, String client){
-        DockerConnection conn = createConnection(packet.getDockerHost());
-        if (conn == null) {
-            packet.setResponse(client, "STATUS_FAILED,HOST_NOT_FOUND");
-            return;
-        }
+        CompletableFuture.runAsync(() -> {
+            DockerConnection conn = createConnection(packet.getDockerHost());
+            if (conn == null) {
+                packet.setResponse(client, "STATUS_FAILED,HOST_NOT_FOUND");
+                return;
+            }
 
-        String containerId = conn.createContainer(packet.getContainerImage(), packet.getExposedPorts(), packet.getEnvVariables());
-        if (containerId == null){
-            packet.setResponse(client, "STATUS_FAILED");
-            return;
-        }
+            String containerId = conn.createContainer(packet.getContainerImage(), packet.getExposedPorts(), packet.getEnvVariables());
+            if (containerId == null){
+                packet.setResponse(client, "STATUS_FAILED");
+                return;
+            }
 
-        InetSocketAddress address;
-        try {
-            address = new InetSocketAddress(packet.getServerAddress(), Integer.parseInt(packet.getServerPort()));
-        }catch (Exception e){
-            StarGate.getInstance().getLogger().warning("ERROR: Unable to create InetAddress!");
-            packet.setResponse(client, "STATUS_FAILED_BAD_IP,"+containerId);
-            return;
-        }
+            InetSocketAddress address;
+            try {
+                address = new InetSocketAddress(packet.getServerAddress(), Integer.parseInt(packet.getServerPort()));
+            }catch (Exception e){
+                StarGate.getInstance().getLogger().warning("ERROR: Unable to create InetAddress!");
+                packet.setResponse(client, "STATUS_FAILED_BAD_IP,"+containerId);
+                return;
+            }
 
-        GateAPI.addServer(packet.getServerName(), address, packet.getServerAddress());
-        packet.setResponse(client, "STATUS_SUCCESS,"+containerId);
+            GateAPI.addServer(packet.getServerName(), address, packet.getServerAddress());
+            packet.setResponse(client, "STATUS_SUCCESS,"+containerId);
+        });
     }
 }
